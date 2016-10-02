a76c62eaa559d77f50b48cfb8a9daa684872dd29
Chinh Nguyen <chinhnguyen@tav.local>
2016-10-02 01:32:41 -0700

    pseudocode 6.6

---
+++ ruby/Word_game/Word_game.rb
@@ -0,0 +1,31 @@
+#word guessing game
+#create a game that lets player 1 enter a word
+#this word will be hidden and char will be replace underscore
+#player 2 will guess by entering 1 letter at a time
+#track incrrect guesses can not excess length of word
+#correct guesses will reveal itself to player by showing on the word
+#incorrect guess will prompt user to try again
+#once all letter is guess prompt winning screen
+#otherwise promopt you lose play again screen
+
+#logic code
+#create game class
+#initial method to split word to array
+#guess will equal length of word
+#set a game over variable to false
+
+#method to change word into (underscore)
+
+#method to check input 
+
+#user inferface
+#WHILE loop to take input
+#IF correct fill in hidden word
+#end game if all letters guessed
+
+#ELSE lose a guess and try again
+#lose game if guess equal 0
+
+
+
+
